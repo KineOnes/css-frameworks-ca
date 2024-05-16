2bd4dda737f4cf8a66475b3be75b05e0fd0d7cb7
KineOnes <kine.verhaaf@gmail.com>
2024-05-16 15:59:02 +0200

    Add required pattern to email input

--- src/js/register.js
+++ src/js/register.js
@@ -43,3 +43,25 @@ function registerFormListener() {
 }
 
 registerFormListener();
+
+async function loginUser(url, userData) {
+    try {
+        const postData = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        };
+        const response = await fetch(url, postData);
+        console.log(response);
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const loginUrl = `${API_BASE_URL}/social/auth/login`;
+
+loginUser(loginUrl);
